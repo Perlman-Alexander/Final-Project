dfd6835c5605a1427d2682338f85b6037632a2cd
Stefan Alexander <sualexander@wpi.edu>
2014-12-09 19:04:30 -0500

    Math.random();

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ window.addEventListener('load', function()
 	scene = new THREE.Scene();
 
 	camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.01, 100000);
-	camera.position.set(0, 0, -2000);
+	camera.position.set(0, 0, -1000);
 	camera.aspect = 2;
 	camera.lookAt(scene.position);
 	scene.add(camera);
@@ -23,24 +23,26 @@ window.addEventListener('load', function()
 	container.appendChild(renderer.domElement);
 	controls = new THREE.OrbitControls(camera);
 
-	var particleCount = 2000;
+	var particleCount = 500;
 	var particlesGeometry = new THREE.Geometry();
-	for (var p = 0; p < particleCount; p++) {
+	for (var p = 0; p < particleCount * 5; p++) {
 		var px = Math.random()*500 - 250;
 		var py = Math.random()*500 - 250;
 		var pz = Math.random()*500 - 250;
 		var particle = new THREE.Vector3(px, py, pz);
+
 		particlesGeometry.vertices.push(particle);
 
 	}
 
 	var colors = [0xffa22b, 0xe87727, 0xff6d37, 0xe83f27, 0xff2b3b];
-	for (var i = 0; i < colors.length; i++) {
-		var particleMaterial = new THREE.PointCloudMaterial({color: colors[i], size: 20});
-		var cloud = new THREE.PointCloud( particlesGeometry, particleMaterial );
+	// var cloud = new THREE.PointCloud( particlesGeometry, particleMaterial );
+	var cloud;
+	for (var i = 0; i < 10; i++) {
+		var particleMaterial = new THREE.PointCloudMaterial({color: colors[(i)%5], size: i+1});
+		cloud = new THREE.PointCloud( particlesGeometry, particleMaterial );
 		scene.add(cloud);
 	}
-
 	
 	// // var particleSystem = new THREE.Geometry(particles, particleMaterial);
 	// var particles = new THREE.PointCloudMaterial(particlesGeometry, particleMaterial);
@@ -53,7 +55,12 @@ function animation() {
 	for (var i = 0; i < scene.children.length; i++) {
 		var object = scene.children[i];
 		if (object instanceof THREE.PointCloud) {
-			object.rotation.y = time * (i < 4 ? i + 1 : -(i + 1));
+			// for (var j = 0; j < object.geometry.vertices.length; j++) {
+			// 	object.geometry.vertices[j].z = time;
+			// }
+			// object.rotation.y = (time * (i < 4 ? i + 1 : -(i + 1)))*0.01;
+
+			object.rotation.y = Math.sin(time)*Math.random()*10;
 		}
 	}
 }
